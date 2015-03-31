34e2e9a2b8dbf5d291b3fcbb6056d23eb8a18866
Adri <acsantome@gmail.com>
2015-03-31 11:28:22 +0200

    Modified the 'CMakeLists.txt' file to 'load' the osgGML-Options lib and execute the test.
    *) More tests [INCOMPLETE] with fixtures, just to play a bit with the testing framework.

--- test/options/testOptions.cpp
+++ test/options/testOptions.cpp
@@ -5,15 +5,43 @@
 
 #include "boost/test/unit_test.hpp"
 
-BOOST_AUTO_TEST_CASE(DefaultOptions)
+/**
+* \brief Simple test to check how Boost.Test works.
+* \todo  Complete the test cases.
+* \todo  Add more test cases.
+*/
+BOOST_FIXTURE_TEST_SUITE(Options, osgGML::GmlOptions)
+
+BOOST_AUTO_TEST_CASE(DefaultGlobalOptions)
 {
-	osgGML::GmlOptions::Ptr opts = new osgGML::GmlOptions;
-	osgGML::GlobalOptions gopts = opts->globalOptions();
+	const osgGML::GlobalOptions& gopts = globalOptions();
 	
 	BOOST_CHECK(gopts.hasCreator());
-	BOOST_CHECK(1 == gopts.getVersion());
-	BOOST_CHECK(true == gopts.isDirected());
+	BOOST_CHECK_EQUAL(gopts.getVersion(), 1);
+	BOOST_CHECK_EQUAL(gopts.isDirected(), true);
 	
 	// Just to see what happens with BOOST_CHECK when it's false.
-	BOOST_CHECK(gopts.hasComment());
+	// BOOST_CHECK(gopts.hasComment());
 }
+
+BOOST_AUTO_TEST_CASE(DefaultGraphicsOptions)
+{
+  const osgGML::GraphicOptions& gropts = graphicOptions();
+
+  BOOST_CHECK(gropts.hasTopology("Drawable"));
+  
+  const osgGML::Topology& topo = gropts.getTopology("StateSet");
+  BOOST_CHECK_EQUAL(topo.getEdge(), "dashed");
+}
+
+BOOST_AUTO_TEST_CASE(Setters)
+{
+	osgGML::GlobalOptions gopts = globalOptions();
+  gopts.setGmlVersion(777);
+  BOOST_CHECK_EQUAL(gopts.getVersion(), 777);
+  
+  // More to come.
+  
+}
+
+BOOST_AUTO_TEST_SUITE_END()
